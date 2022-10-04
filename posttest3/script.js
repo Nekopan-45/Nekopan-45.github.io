4c767ca6b0aeaa31bc4e49656ba4dec3d04b31b1
Nekopan-45 <74187496+Nekopan-45@users.noreply.github.com>
2022-10-04 22:46:54 +0800

    Add files via upload

---
+++ posttest3/script.js
@@ -0,0 +1,20 @@
+function home2(){
+    alert("Talent akan segera hadir");
+}
+
+function func(){
+    var kepala = document.getElementsByClassName("header");
+    element.classList.toggle("dark-mode");
+}
+
+const buttonA = document.getElementById("btnA");
+buttonA.addEventListener("click", function(){
+    document.getElementById("agency").innerHTML = "Segera Datang List Agency";
+});
+
+function removeAtt(){
+    document.getElementsByTagName("H1")[0].removeAttribute("class");
+}
+
+const isi = document.getElementById("konten")
+isi.style.color = 'green';
